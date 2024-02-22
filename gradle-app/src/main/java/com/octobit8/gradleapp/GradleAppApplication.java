62e2a6f63aa0d64e49a42573868410564928ab2d
Octobit8 <abhishek.srivastava@octobit8.com>
2024-02-22 12:24:35 +0530

    Initial draft

---
+++ gradle-app/src/main/java/com/octobit8/gradleapp/GradleAppApplication.java
@@ -0,0 +1,13 @@
+package com.octobit8.gradleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleAppApplication.class, args);
+	}
+
+}
---
+++ gradle-app/src/test/java/com/octobit8/gradleapp/GradleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octobit8.gradleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
